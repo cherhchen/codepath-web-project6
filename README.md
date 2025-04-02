477682f34c3d16685bb3ced4659e22ed1e7250ba
cherhchen <54284307+cherhchen@users.noreply.github.com>
2025-04-02 10:18:53 -0700

    completed project

---
+++ README.md
@@ -0,0 +1,71 @@
+# Web Development Project 5 - *Brewery Data Dashboard*
+
+Submitted by: **Cheryl Chen**
+
+This web app uses the Open Brewery DB API to query for random breweries and displays them in a table as well as show summary statistics.
+
+Time spent: **3** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [X] **The site has a dashboard displaying a list of data fetched using an API call**
+  - The dashboard should display at least 10 unique items, one per row
+  - The dashboard includes at least two features in each row
+- [X] **`useEffect` React hook and `async`/`await` are used**
+- [X] **The app dashboard includes at least three summary statistics about the data** 
+  - The app dashboard includes at least three summary statistics about the data, such as:
+    - The total number of results
+    - The number of US breweries in the results
+    - The number of foreign breweries in the results
+- [X] **A search bar allows the user to search for an item in the fetched data**
+  - The search bar **correctly** filters items in the list, only displaying items matching the search query
+  - The list of results dynamically updates as the user types into the search bar
+- [X] **An additional filter allows the user to restrict displayed items by specified categories**
+  - The filter restricts items in the list using a **different attribute** than the search bar 
+  - The filter **correctly** filters items in the list, only displaying items matching the filter attribute in the dashboard
+  - The dashboard list dynamically updates as the user adjusts the filter
+
+The following **optional** features are implemented:
+
+- [X] Multiple filters can be applied simultaneously
+- [X] Filters use different input types
+  - e.g., as a text input, a dropdown or radio selection, and/or a slider
+- [ ] The user can enter specific bounds for filter values
+
+The following **additional** features are implemented:
+
+* [ ] List anything else that you added to improve the site's functionality!
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented features:
+
+<img src='./data-dashboard-demo.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+<!-- Replace this with whatever GIF tool you used! -->
+GIF created with LICEcap
+<!-- Recommended tools:
+[Kap](https://getkap.co/) for macOS
+[ScreenToGif](https://www.screentogif.com/) for Windows
+[peek](https://github.com/phw/peek) for Linux. -->
+
+## Notes
+Figuring how to implement the feature to allow for simulataneous filtering was a bit challenging logic-wise. 
+
+## License
+
+    Copyright [2025] [Cheryl Chen]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ src/App.jsx
@@ -0,0 +1,163 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [list, setList] = useState(null);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [typeList, setTypeList] = useState(null);
+  const [selectedType, setSelectedType] = useState("");
+  const [numUSBreweries, setNumUSBreweries] = useState(0);
+  const [numForeignBreweries, setNumForeignBreweries] = useState(0);
+
+  useEffect(() => {
+    const getBreweries = async () => {
+      const url = "https://api.openbrewerydb.org/v1/breweries/random?size=20";
+      const response = await fetch(url);
+      const json = await response.json();
+      setList(json);
+      const availableTypes = json.map((brewery) =>
+        brewery.brewery_type
+      );
+      setTypeList([...new Set(availableTypes)]);
+      setNumUSBreweries(countUSBreweries(json));
+      setNumForeignBreweries(countForeignBreweries(json));
+    }
+    getBreweries().catch(console.error);
+  }, []);
+
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    let filteredData = [];
+    if (searchValue != "") {
+      filteredData = list.filter((brewery) => 
+        brewery.name.toLowerCase().includes(searchValue)
+      );
+    }
+    else {
+      // Reset filteredData if search input is blank
+      filteredData = list;
+    }
+    if (selectedType != "") {
+      filteredData = filteredData.filter((brewery) => 
+        brewery.brewery_type === selectedType
+      );
+    }
+    setFilteredResults(filteredData);
+    setNumUSBreweries(countUSBreweries(filteredData));
+    setNumForeignBreweries(countForeignBreweries(filteredData));
+  }
+
+  const handleSelectChange = (event) => {
+    setSelectedType(event.target.value);
+    let filteredData = [];
+    if (event.target.value != "") {
+      filteredData = list.filter((brewery) => 
+        brewery.brewery_type === event.target.value
+      );
+    }
+    if (searchInput != "") {
+      filteredData = filteredData.filter((brewery) => 
+        brewery.name.toLowerCase().includes(searchInput)
+      );
+    }
+    setFilteredResults(filteredData);
+    setNumUSBreweries(countUSBreweries(filteredData));
+    setNumForeignBreweries(countForeignBreweries(filteredData));
+  }
+
+  const countUSBreweries = (data) => {
+    const USBreweries = data.filter((brewery) => 
+      brewery.country === "United States"
+    );
+    return USBreweries.length;
+  }
+
+  const countForeignBreweries = (data) => {
+    const USBreweries = data.filter((brewery) => 
+      brewery.country !== "United States"
+    );
+    return USBreweries.length;
+  }
+
+  console.log(list);
+  // console.log(typeList);
+  return (
+    <div>
+      <h1>Brewery Data Dashboard</h1>
+      {list &&
+        <div className="stats-container">
+          <div className="stats">
+            <h2>Total Results ✅</h2>
+            <p className="stats-text">{searchInput.length > 0 || selectedType != "" ? filteredResults.length : list.length}</p>
+          </div>
+          <div className="stats">
+            <h2>US Breweries 🇺🇸</h2>
+            <p className="stats-text">{numUSBreweries}</p>
+          </div>
+          <div className="stats">
+            <h2>Foreign Breweries 🌐</h2>
+            <p className="stats-text">{numForeignBreweries}</p>
+          </div>
+        </div>
+      }
+      <h2>List of Breweries 🍺</h2>
+      <div className="filter-container">
+        <input
+          type="text"
+          placeholder="Search by Name"
+          onChange={(inputString) => searchItems(inputString.target.value)}
+        />
+        <div>
+          <label name="brewery-type">Type: </label>
+          <select name="Brewery Type" id="brewery-type" value={selectedType} onChange={handleSelectChange}>
+            <option value=""></option>
+            { typeList && typeList.map((item) => 
+              <option value={item} key={item}>{item}</option>
+              )
+            }
+          </select>
+        </div>
+      </div>
+      <div className="table-container">
+        {list &&
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+                <th>City</th>
+                <th>State</th>
+                <th>Country</th>
+              </tr>
+            </thead>
+            <tbody>
+            {searchInput.length > 0 || selectedType != "" ? 
+              filteredResults.map((brewery) => (
+                <tr key={brewery.id}>
+                <td>{brewery.name}</td>
+                <td>{brewery.brewery_type}</td>
+                <td>{brewery.city}</td>
+                <td>{brewery.state}</td>
+                <td>{brewery.country}</td>
+              </tr>
+            )) : (
+              list.map((brewery) => (
+                <tr key={brewery.id}>
+                  <td>{brewery.name}</td>
+                  <td>{brewery.brewery_type}</td>
+                  <td>{brewery.city}</td>
+                  <td>{brewery.state}</td>
+                  <td>{brewery.country}</td>
+                </tr>
+              )))
+            }
+            </tbody>
+          </table>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
